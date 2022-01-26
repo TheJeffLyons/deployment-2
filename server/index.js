27deb19efb99b78c6f68edfe12e01fe3111eda62
Jeff Lyons <jeffreylyons@outlook.com>
2022-01-26 18:42:31 -0500

    commit G

--- server/index.js
+++ server/index.js
@@ -4,16 +4,15 @@ const path = require('path')
 const port = process.env.PORT || process.env.SERVER_PORT
 
 const app = express()
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
-app.use(express.json())
+app.use('/js', express.static(path.join(__dirname, '/main.js')))
 
-app.use('/js', express.static(path.join(__dirname, 'public/main.js')))
-
-app.use('/styles', express.static(path.join(__dirname, '/index.css')))
+app.use('/css', express.static(path.join(__dirname, '/main.css')))
 
 app.listen(port, () => {
     console.log('Listening on port ' + port)
